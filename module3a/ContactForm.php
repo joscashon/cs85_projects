f2cd69f6c6ce5813e52d7c26d696e9422645ce5b
Jos Cashon <joscashon@gmail.com>
2025-07-06 16:06:01 -0700

    Add displayForm function with documentation. Pretty sure I typed it correctly as showin in the assignment page, but Intelliphense is complaining about "Unexpected '<'." at the closing html body tag, so this might not be working.

--- module3a/ContactForm.php
+++ module3a/ContactForm.php
@@ -40,6 +40,35 @@ function validateEmail($data, $fieldname) {
     }
     return ($retval);
 }
-?>
+
+// This function will display the form with the current values of the input fields
+// It will use the values passed as parameters to pre-fill the form fields
+function displayForm($Sender, $Email, $Subject, $Message) {
+    ?> <h2 style = "text-align:center">Contact Me</h2>
+    <form name ="contact" action="ContactForm.php" method="post">
+        <p>Your Name: 
+            <input type="text" name="Sender" value="<?php echo $Sender; ?>" /></p>
+        <p>Your Email: 
+            <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
+        <p>Subject: 
+            <input type="text" name="Subject" value="<?php echo $Subject; ?>" /></p>
+        <p>Message: <br />
+            <textarea name="Message"><?php echo $Message; ?></textarea></p>
+        <p><input type="reset" value="Clear Form" />&nbsp; &nbsp;
+            <input type="submit" name="submit" value="Send Form" /></p>
+    </form>
+
+<?php }
+
+// Initialize variables
+$ShowForm = TRUE;
+$errorCount = 0;
+$Sender = "";
+$Email = "";
+$Subject = "";
+$Message = "";
+
+
+
 </body>
 </html>
